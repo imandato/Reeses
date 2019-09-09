20e36f4c8ee8ce302491e8985210e753bf64ac8a
Ivelisse Mandato <mandatoi@bc.edu>
2019-09-09 18:48:21 -0400

    assignment completed

--- Reeses/ViewController.swift
+++ Reeses/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func heyButtonPressed(_ sender: UIButton) {
+        let message1 = "You got peanut butter in my chocolate"
+        let message2 = "You got chocolate in my peanut butter"
+        if messageLabel.text == message1 {
+            messageLabel.text = message2
+            messageLabel.textAlignment = .left
+            messageLabel.textColor = .brown
+        } else {
+            messageLabel.text = message1
+            messageLabel.textAlignment = .right
+            messageLabel.textColor = .orange
+        }
+    }
 }
 
